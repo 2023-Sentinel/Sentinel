37eefc0e38ac6dbd040b11512ff56f903564c189
김태환 <appleit1245@naver.com>
2023-05-05 20:24:28 +0900

    changed asset

--- src/assets/AtkNode.js
+++ src/assets/AtkNode.js
@@ -1,14 +1,18 @@
 //Attacker Node
 import { NodeBuilder } from '@baklavajs/core'
 
-export const AtkNode = new NodeBuilder('AtkNode')
+export const AtkNode = new NodeBuilder('AtkNode',{
+    twoColumn: true,
+    width: 300
+})
     //노드 이름 설정
     .setName('Attacker')
     //노트 인풋 연결 Interface
     .addInputInterface('InputNode')
+    .addOutputInterface('OutputNode')
     .addOption('Attacker Name', 'InputOption')
     .addOption('Date', 'InputOption')
-    .addOutputInterface('OutputNode')
+
     .build()
 
 
--- src/assets/PCNode.js
+++ src/assets/PCNode.js
@@ -1,16 +1,22 @@
 import { NodeBuilder } from '@baklavajs/core'
 
-export const PCNode = new NodeBuilder('PCNode')
+export const PCNode = new NodeBuilder('PCNode',{
+    twoColumn:true,
+    width: 300
+})
     //노드 이름 설정
     .setName('PC')
     //노트 인풋 연결 Interface
     .addInputInterface('InputNode')
+    .addOutputInterface('OutputNode')
     .addOption('IPAddress', 'InputOption')
     .addOption('Date', 'InputOption')
+
+
     /*
     .addOption('Operation', 'SelectOption', 'Add', undefined, {
         items: ['Add', 'Subtract']
     })*/
 
-    .addOutputInterface('OutputNode')
+
     .build()
