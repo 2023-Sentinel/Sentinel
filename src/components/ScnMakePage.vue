bf0e09d098ce44bd036288f4039d31310cc81ff5
김태환 <appleit1245@naver.com>
2023-05-05 20:27:20 +0900

    changed asset

--- src/components/ScnMakePage.vue
+++ src/components/ScnMakePage.vue
@@ -1,7 +1,7 @@
 <template>
-    <button class="custom-btn Aplbutton" type="button" @click="srnSave()"><span>Apply</span>
+    <button class="custom-btn Aplbutton" type="button" @click="srnSave()"><span>Save</span>
     </button>
-    <button class="Loadbutton" type="button" @click="srnLoad()"><span>Load</span></button>
+    <button class="custom-btn Ldbutton" type="button" @click="srnLoad()"><span>Load</span></button>
 
     <div style="height: 80vh; width: 80vw">
         <baklava-editor :plugin="viewPlugin" />
@@ -21,6 +21,7 @@ import {PCNode} from '@/assets/PCNode'
 import {SVNode} from '@/assets/ServerNode'
 import {FLNode} from '@/assets/FileNode'
 import {AccountNode} from '@/assets/AcntNode'
+import router from "@/scripts/router";
 
 
 export default {
@@ -28,6 +29,9 @@ export default {
         srnSave: function() {
             const IState=this.editor.save()
             console.log(IState)
+            router.push({
+                path:"/Dashboard"
+            })
         },
         srnLoad: function() {
             let IState2=this.editor.save()
@@ -155,5 +159,76 @@ export default {
     width: 100%;
 }
 
+.Ldbutton {
+    background: rgb(0,172,238);
+    background: linear-gradient(0deg, rgba(0,172,238,1) 0%, rgba(2,126,251,1) 100%);
+    width: 150px;
+    height: 40px;
+    line-height: 42px;
+    padding: 0;
+    border: none;
+    margin-right: 3px;
+    position: relative;
+
+}
+.Ldbutton span {
+    position: relative;
+    display: block;
+    width: 100%;
+    height: 100%;
+}
+.Ldbutton:before,
+.Ldbutton:after {
+    position: absolute;
+    content: "";
+    right: 0;
+    top: 0;
+    background: rgba(2,126,251,1);
+    transition: all 0.3s ease;
+}
+.Ldbutton:before {
+    height: 0%;
+    width: 2px;
+}
+.Ldbutton:after {
+    width: 0%;
+    height: 2px;
+}
+.Ldbutton:hover{
+    background: transparent;
+    box-shadow: none;
+}
+.Ldbutton:hover:before {
+    height: 100%;
+}
+.Ldbutton:hover:after {
+    width: 100%;
+}
+.Ldbutton span:hover{
+    color: rgba(2,126,251,1);
+}
+.Ldbutton span:before,
+.Ldbutton span:after {
+    position: absolute;
+    content: "";
+    left: 0;
+    bottom: 0;
+    background: rgba(2,126,251,1);
+    transition: all 0.3s ease;
+}
+.Ldbutton span:before {
+    width: 2px;
+    height: 0%;
+}
+.Ldbutton span:after {
+    width: 0%;
+    height: 2px;
+}
+.Ldbutton span:hover:before {
+    height: 100%;
+}
+.Ldbutton span:hover:after {
+    width: 100%;
+}
 
 </style>
